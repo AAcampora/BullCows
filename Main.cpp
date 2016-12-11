a55e22bcbc90aca41a9e829dcf06c0e649927b42
alessio-PC\alessio <acampora199@gmail.com>
2016-12-11 19:59:19 +0000

    Initial commit

---
+++ Main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+void PrintIntro(int word);
+string GetGuess();
+void RepeatGuess(string guess);
+
+//the entry point of our operator 
+int main()
+{
+	constexpr int WORD_LENGTH = 9;
+	PrintIntro(WORD_LENGTH);
+
+
+	RepeatGuess(GetGuess());
+	RepeatGuess(GetGuess());
+
+
+	return 0;
+}
+void PrintIntro(int word)
+{
+	//prints on screen the intrudutcion
+	cout << "Welcome to Bulls and Cows, a fun word game \n";
+	cout << "Can you guess the " << word;
+	cout << (" letter isogram i'm thinking of? \n");
+	cout << endl;
+	return;
+}
+
+string GetGuess()
+{
+	//Asks Guess to the player
+	cout << "Please enter your guess: \n ";
+	string Guess = "";
+	getline(cin, Guess);
+
+	return Guess;
+}
+
+void RepeatGuess(string guess)
+{
+	//Repeat guess to the player
+	cout << "Your guess was: " << guess << endl;
+}
